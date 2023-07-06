c9a301ca38cc85a114fe429c4b03bbdc6a4508b1
Nestor162 <nestorcicardini@gmail.com>
2023-07-06 16:30:09 +0200

    Implement routing system with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import LoginPage from './components/auth/LoginPage'
+import RegisterPage from './components/auth/RegisterPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
     <>
-      <div className='dark-bg'>
-        <div id='auth-wrapper'>
-          <LoginPage />
-        </div>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='*' element={<h1>Not found</h1>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
