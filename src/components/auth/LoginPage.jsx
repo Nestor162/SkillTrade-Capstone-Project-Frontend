42feb25cb1f9313167c5c415b42b38002543cc93
Nestor162 <nestorcicardini@gmail.com>
2023-07-06 16:31:55 +0200

    Add wrapper elements

--- src/components/auth/LoginPage.jsx
+++ src/components/auth/LoginPage.jsx
@@ -1,20 +1,24 @@
 import LoginForm from './LoginForm'
 import GoogleSignBtn from './GoogleSignBtn'
 import AuthHeader from './AuthHeader'
+import { Link } from 'react-router-dom'
+
 function LoginPage() {
   return (
-    <>
-      <AuthHeader title='Welcome!' subtitle='Login to continue' />
-      <LoginForm />
-      <div className='d-flex align-items-center justify-content-center mt-4'>
-        <div className='horizontal-separator ms-5'></div> <span className='mx-3 align-self-center'>OR</span>
-        <div className='horizontal-separator me-5'></div>
+    <div className='dark-bg'>
+      <div className='centered-overlay-box'>
+        <AuthHeader title='Welcome!' subtitle='Login to continue' />
+        <LoginForm />
+        <div className='d-flex align-items-center justify-content-center mt-4'>
+          <div className='horizontal-separator ms-5'></div> <span className='mx-3 align-self-center'>OR</span>
+          <div className='horizontal-separator me-5'></div>
+        </div>
+        <GoogleSignBtn />
+        <p className='d-flex justify-content-center mt-2'>
+          New to SkillTrade?&nbsp;<Link to='/register'>Register</Link>
+        </p>
       </div>
-      <GoogleSignBtn />
-      <p className='d-flex justify-content-center mt-2'>
-        New to SkillTrade?&nbsp;<a href='#'>Register</a>
-      </p>
-    </>
+    </div>
   )
 }
 
