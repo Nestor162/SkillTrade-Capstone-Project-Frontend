e467088f317e0568bc245133afddba6f6c5f2e2f
Nestor162 <nestorcicardini@gmail.com>
2023-07-07 17:05:32 +0200

    Add icon to show/hide password in login page

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,3 +1,4 @@
+import { Eye, EyeOff } from 'lucide-react'
 import { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
@@ -13,6 +14,9 @@ function LoginForm() {
     userLoginPayload.password = password
     console.log(userLoginPayload)
   }
+
+  // ---- PASSWORD SHOW/HIDE ----
+  const [hidePassword, setHidePassword] = useState(true)
 
   return (
     <Form className='d-flex flex-column mx-4 mt-4' onSubmit={handleSubmit}>
@@ -30,14 +34,24 @@ function LoginForm() {
 
       <Form.Group className='mb-3' controlId='formBasicPassword'>
         <Form.Label>Password</Form.Label>
-        <Form.Control
-          type='password'
-          placeholder='Enter your password'
-          onChange={event => {
-            setPassword(event.target.value)
-          }}
-          value={password}
-        />
+        <div className='position-relative'>
+          <Form.Control
+            type={hidePassword ? 'password' : 'text'}
+            placeholder='Enter your password'
+            onChange={event => {
+              setPassword(event.target.value)
+            }}
+            value={password}
+          />
+          <Eye
+            className={hidePassword ? 'd-none' : 'position-absolute top-50 end-0 translate-middle-y me-2'}
+            onClick={() => setHidePassword(true)}
+          />
+          <EyeOff
+            className={hidePassword ? 'position-absolute top-50 end-0 translate-middle-y me-2' : 'd-none'}
+            onClick={() => setHidePassword(false)}
+          />
+        </div>
       </Form.Group>
       <Button type='submit' className='mt-3 main-btn'>
         LOGIN
