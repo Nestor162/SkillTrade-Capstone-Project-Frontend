3241c6c8f16f21ff16c9aad846f49bf9bd88de67
Nestor162 <nestorcicardini@gmail.com>
2023-07-07 18:39:38 +0200

    Implement login fetch

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,5 +1,6 @@
 import { Eye, EyeOff } from 'lucide-react'
 import { useState } from 'react'
+import { Alert, OverlayTrigger, Popover } from 'react-bootstrap'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 
@@ -13,10 +14,31 @@ function LoginForm() {
     userLoginPayload.email = email
     userLoginPayload.password = password
     console.log(userLoginPayload)
+    registerUser(userLoginPayload)
+    setTimeout(() => setErrorMsg(null), 3000)
   }
 
   // ---- PASSWORD SHOW/HIDE ----
   const [hidePassword, setHidePassword] = useState(true)
+
+  // ---- LOGIN FETCH ----
+  async function registerUser(payload) {
+    const response = await fetch('http://localhost:3001/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+    const data = await response.json()
+
+    if (!response.ok) {
+      setErrorMsg(data.message)
+    }
+  }
+
+  // ERRROR ALERT SHOW/HIDE
+  const [errorMsg, setErrorMsg] = useState(null)
 
   return (
     <Form className='d-flex flex-column mx-4 mt-4' onSubmit={handleSubmit}>
@@ -29,6 +51,7 @@ function LoginForm() {
             setEmail(event.target.value)
           }}
           value={email}
+          required
         />
       </Form.Group>
 
@@ -42,6 +65,7 @@ function LoginForm() {
               setPassword(event.target.value)
             }}
             value={password}
+            required
           />
           <Eye
             className={hidePassword ? 'd-none' : 'position-absolute top-50 end-0 translate-middle-y me-2'}
@@ -53,9 +77,25 @@ function LoginForm() {
           />
         </div>
       </Form.Group>
-      <Button type='submit' className='mt-3 main-btn'>
-        LOGIN
-      </Button>
+
+      <OverlayTrigger
+        show={errorMsg !== null}
+        placement='bottom'
+        overlay={
+          <Popover id='popover-basic'>
+            <Popover.Header as='h3' style={{ backgroundColor: 'var(--tertiary-color-light)' }}>
+              <span className='text-light'>LOGIN ERROR</span>
+            </Popover.Header>
+            <Popover.Body className='bg-danger-muted' style={{ backgroundColor: 'var(--tertiary-color)' }}>
+              <span className='text-light'> {errorMsg}</span>
+            </Popover.Body>
+          </Popover>
+        }
+      >
+        <Button type='submit' className='mt-3 main-btn'>
+          LOGIN
+        </Button>
+      </OverlayTrigger>
     </Form>
   )
 }
