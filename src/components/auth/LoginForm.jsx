c770f33e1b628a25e17c44316f227307e184ddfa
Nestor162 <nestorcicardini@gmail.com>
2023-07-07 13:11:16 +0200

    Get values of login input fields

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,17 +1,43 @@
+import { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 
 function LoginForm() {
+  const userLoginPayload = { email: '', password: '' }
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    userLoginPayload.email = email
+    userLoginPayload.password = password
+    console.log(userLoginPayload)
+  }
+
   return (
-    <Form className='d-flex flex-column mx-4 mt-4'>
+    <Form className='d-flex flex-column mx-4 mt-4' onSubmit={handleSubmit}>
       <Form.Group className='mb-3' controlId='formBasicEmail'>
         <Form.Label>Email</Form.Label>
-        <Form.Control type='email' placeholder='Enter your email' />
+        <Form.Control
+          type='email'
+          placeholder='Enter your email'
+          onChange={event => {
+            setEmail(event.target.value)
+          }}
+          value={email}
+        />
       </Form.Group>
 
       <Form.Group className='mb-3' controlId='formBasicPassword'>
         <Form.Label>Password</Form.Label>
-        <Form.Control type='password' placeholder='Enter your password' />
+        <Form.Control
+          type='password'
+          placeholder='Enter your password'
+          onChange={event => {
+            setPassword(event.target.value)
+          }}
+          value={password}
+        />
       </Form.Group>
       <Button type='submit' className='mt-3 main-btn'>
         LOGIN
