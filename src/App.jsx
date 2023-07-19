b50d02ce6ab60bf37153bc987de8a9d802a1b76e
Nestor162 <nestorcicardini@gmail.com>
2023-07-19 18:09:51 +0200

    Add protected routes to avoid accessing pages without logging in first

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,23 @@ import ProfileNameSurnamePage from './components/profile_setup/ProfileNameSurnam
 import Homepage from './components/home/Homepage'
 import PostDetails from './components/details/PostDetails'
 import ProfileDetails from './components/details/ProfileDetails'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+import PropTypes from 'prop-types'
+
+const ProtectedRoute = ({ children }) => {
+  const navigate = useNavigate()
+  const token = localStorage.getItem('token')
+  useEffect(() => {
+    if (!token) {
+      navigate('/')
+    }
+  }, [navigate, token])
+  if (!token) {
+    return null
+  }
+  return children
+}
 
 function App() {
   return (
@@ -16,17 +33,62 @@ function App() {
         <Routes>
           <Route path='/' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage />} />
-          <Route path='/interests' element={<InterestsSelectionPage />} />
-          <Route path='/profile-name' element={<ProfileNameSurnamePage />} />
-          <Route path='/profile-creation' element={<ProfileCreation />} />
-          <Route path='/home' element={<Homepage />} />
-          <Route path='/post-details' element={<PostDetails />} />
-          <Route path='/profile-details' element={<ProfileDetails />} />
+          <Route
+            path='/interests'
+            element={
+              <ProtectedRoute>
+                <InterestsSelectionPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/profile-name'
+            element={
+              <ProtectedRoute>
+                <ProfileNameSurnamePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/profile-creation'
+            element={
+              <ProtectedRoute>
+                <ProfileCreation />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/home'
+            element={
+              <ProtectedRoute>
+                <Homepage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/post-details'
+            element={
+              <ProtectedRoute>
+                <PostDetails />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/profile-details'
+            element={
+              <ProtectedRoute>
+                <ProfileDetails />
+              </ProtectedRoute>
+            }
+          />
           <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
   )
+}
+ProtectedRoute.propTypes = {
+  children: PropTypes.node.isRequired
 }
 
 export default App
