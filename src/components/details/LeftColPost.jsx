f01ea42df2cc92fa85862cfd08f3fa98a793b5fc
Nestor162 <nestorcicardini@gmail.com>
2023-07-19 12:01:33 +0200

    Update profile info left Col on post details page
    Updated col to display below and centered on mobile

--- src/components/details/LeftColPost.jsx
+++ src/components/details/LeftColPost.jsx
@@ -7,40 +7,42 @@ import StarRatings from 'react-star-ratings'
 
 function LeftColPost({ name, surname, profilePicture, biography, averageRating, location, profileId }) {
   return (
-    <Col xs={4} className='d-none d-md-flex'>
-      <Card className='left-col-details ms-5 mt-4 border-0 position-sticky' style={{ top: '95px' }}>
+    <Col xs={12} md={4} className='d-flex flex-column flex-md-row order-1 order-md-0'>
+      <Card className='left-col-details mx-auto ms-md-5 mt-4 border-0 position-sticky' style={{ top: '95px' }}>
         <Link to={`/profile-details?id=${profileId}`} className='text-decoration-none'>
-          <Card.Body className='text-dark'>
-            <Image
-              src={profilePicture ? profilePicture : ProfilePicturePlaceholder}
-              roundedCircle
-              className='profile-picture-placeholder d-block mx-auto'
-              width={'70px'}
-            />
-            <div className='text-center'>
-              <div className='d-flex justify-content-center align-items-center gap-1s mt-1'>
-                <span className='position-relative'>
-                  <MapPin size={'15px'} className='position-absolute' style={{ right: '45px', top: '4px' }} />
-                  {location}
-                </span>
+          <div className='d-flex justify-content-center'>
+            <Card.Body className='text-dark'>
+              <Image
+                src={profilePicture ? profilePicture : ProfilePicturePlaceholder}
+                roundedCircle
+                className='profile-picture-placeholder d-block mx-auto'
+                width={'70px'}
+              />
+              <div className='text-center'>
+                <div className='d-flex justify-content-center align-items-center gap-1s mt-1'>
+                  <span className='position-relative'>
+                    <MapPin size={'15px'} className='position-absolute' style={{ right: '45px', top: '4px' }} />
+                    {location}
+                  </span>
+                </div>
+                <div className='d-flex align-items-end justify-content-center'>
+                  <StarRatings
+                    rating={averageRating}
+                    starRatedColor='var(--tertiary-color)'
+                    numberOfStars={5}
+                    starDimension='20px'
+                    starSpacing='2px'
+                  />
+                  {averageRating !== 0 && <div className='text-muted small mt-2'>({averageRating.toFixed(1)})</div>}
+                </div>
+                {averageRating === 0 && <div className='text-muted small mt-1 d-block'>(not rated yet)</div>}
               </div>
-              <div className='d-flex align-items-end justify-content-center'>
-                <StarRatings
-                  rating={averageRating}
-                  starRatedColor='var(--tertiary-color)'
-                  numberOfStars={5}
-                  starDimension='20px'
-                  starSpacing='2px'
-                />
-                {averageRating !== 0 && <div className='text-muted small mt-2'>({averageRating.toFixed(1)})</div>}
-              </div>
-              {averageRating === 0 && <div className='text-muted small mt-1 d-block'>(not rated yet)</div>}
-            </div>
-            <hr className='border border-success border-1 opacity-50' />
+              <hr className='border border-success border-1 opacity-50' />
 
-            <Card.Title className='text-dark hover-underline'>{name + ' ' + surname}</Card.Title>
-            <Card.Text className='fs-6'>{biography}</Card.Text>
-          </Card.Body>
+              <Card.Title className='text-dark hover-underline'>{name + ' ' + surname}</Card.Title>
+              <Card.Text className='fs-6'>{biography}</Card.Text>
+            </Card.Body>
+          </div>
         </Link>
       </Card>
     </Col>
