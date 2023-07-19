330ea4cd7c4c1e607de8699ba3a1fe573d30e15a
Nestor162 <nestorcicardini@gmail.com>
2023-07-19 16:42:40 +0200

    Improve left menu layout
    Add animations and tooltips on hover

--- src/components/home/LeftOffCanvasMenu.jsx
+++ src/components/home/LeftOffCanvasMenu.jsx
@@ -4,7 +4,7 @@ import Offcanvas from 'react-bootstrap/Offcanvas'
 import logo from '../../assets/img/skilltrade-logo-only-text-cropped.png'
 import { LogOut, MessagesSquare, Moon, Settings2 } from 'lucide-react'
 import { History } from 'lucide-react'
-import { Form } from 'react-bootstrap'
+import { Form, OverlayTrigger, Tooltip } from 'react-bootstrap'
 
 function LeftOffCanvasMenu() {
   const [show, setShow] = useState(false)
@@ -14,11 +14,23 @@ function LeftOffCanvasMenu() {
 
   const [darkMode, setDarkMode] = useState(false)
 
+  // Dark mode, not working yet
+  useEffect(() => {
+    const savedColorMode = localStorage.getItem('theme')
+    if (savedColorMode === 'dark') {
+      setDarkMode(true)
+    } else {
+      setDarkMode(false)
+    }
+  }, [])
+
   useEffect(() => {
     if (darkMode) {
       document.documentElement.setAttribute('data-color-mode', 'dark')
+      localStorage.setItem('theme', 'dark')
     } else {
       document.documentElement.removeAttribute('data-color-mode')
+      localStorage.setItem('theme', 'light')
     }
   }, [darkMode])
 
@@ -33,47 +45,57 @@ function LeftOffCanvasMenu() {
           <img src={logo} width={'170px'} height={'51.5px'} className='mx-auto mt-0' />
         </Offcanvas.Header>
         <Offcanvas.Body>
-          <div className='left-menu d-flex flex-column gap-4 pt-3 h-100'>
+          <div className='left-menu d-flex flex-column gap-4 pt-3 h-100 ms-4'>
             <div>
               <User size={20} />
               <span>My Profile</span>
             </div>
             <div>
               <Bell size={20} />
-              <span>Notifications</span>
+              <OverlayTrigger placement='right' overlay={<Tooltip id='notifications-tooltip'>Coming soon!</Tooltip>}>
+                <span>Notifications</span>
+              </OverlayTrigger>
             </div>
+
             <div>
               <Settings2 size={20} />
-              <span>Settings</span>
+              <OverlayTrigger placement='right' overlay={<Tooltip id='notifications-tooltip'>Coming soon!</Tooltip>}>
+                <span>Settings</span>
+              </OverlayTrigger>
             </div>
             <div>
               <History size={20} />
-              <span>History</span>
+              <OverlayTrigger placement='right' overlay={<Tooltip id='notifications-tooltip'>Coming soon!</Tooltip>}>
+                <span>History</span>
+              </OverlayTrigger>
             </div>
             <div>
               <MessagesSquare size={20} />
-              <span>Chats</span>
+              <OverlayTrigger placement='right' overlay={<Tooltip id='notifications-tooltip'>Coming soon!</Tooltip>}>
+                <span>Chats</span>
+              </OverlayTrigger>
             </div>
 
-            <div className='d-flex flex-column align-items-start mt-auto'>
-              <div className='d-flex align-items-center'>
-                {darkMode ? <Moon size={20} /> : <Sun size={20} />}
-                <span className='ms-2' style={{ width: '81px' }}>
-                  {darkMode ? 'Dark mode' : 'Light mode'}
-                </span>
-                <Form.Check
-                  type='switch'
-                  id='dark-mode-switch'
-                  className='ms-auto'
-                  onChange={() => setDarkMode(!darkMode)}
-                />
-              </div>
+            <span className='d-flex flex-column align-items-start mt-auto gap-3'>
+              <OverlayTrigger placement='right' overlay={<Tooltip id='notifications-tooltip'>Coming soon!</Tooltip>}>
+                <div className='d-flex align-items-center'>
+                  {darkMode ? <Moon size={20} /> : <Sun size={20} />}
+                  <span style={{ width: '81px' }}>{darkMode ? 'Dark mode' : 'Light mode'}</span>
+                  <Form.Check
+                    disabled
+                    type='switch'
+                    id='dark-mode-switch'
+                    className='ms-auto'
+                    onChange={() => setDarkMode(!darkMode)}
+                  />
+                </div>
+              </OverlayTrigger>
 
               <div>
                 <LogOut size={20} />
                 <span>Logout</span>
               </div>
-            </div>
+            </span>
           </div>
         </Offcanvas.Body>
       </Offcanvas>
