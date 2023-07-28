f498e6a0800adec753a948dc66966b96ed9a7c77
Nestor162 <nestorcicardini@gmail.com>
2023-07-28 10:22:10 +0200

    Conditional routing with react-router-dom
    If there is a token in local storage redirect to homepage, otherwise redirect to login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,36 @@
 import NotFound from './components/NotFound'
 import LoginPage from './components/auth/LoginPage'
 import RegisterPage from './components/auth/RegisterPage'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import InterestsSelectionPage from './components/profile_setup/InterestsSelectionPage'
 import ProfileCreation from './components/profile_setup/ProfileCreationPage'
 import ProfileNameSurnamePage from './components/profile_setup/ProfileNameSurnamePage'
 import Homepage from './components/home/Homepage'
 import PostDetails from './components/details/PostDetails'
 import ProfileDetails from './components/details/ProfileDetails'
-import { useNavigate } from 'react-router-dom'
-import { useEffect } from 'react'
-import PropTypes from 'prop-types'
 import MyProfile from './components/details/MyProfile'
 
-const ProtectedRoute = ({ children }) => {
-  const navigate = useNavigate()
-  const token = localStorage.getItem('token')
-  useEffect(() => {
-    if (!token) {
-      navigate('/')
-    }
-  }, [navigate, token])
-  if (!token) {
-    return null
-  }
-  return children
-}
-
 function App() {
+  const token = localStorage.getItem('token')
+
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<LoginPage />} />
-          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/' element={token ? <Navigate to='/home' /> : <LoginPage />} />
+          <Route path='/register' element={token ? <Navigate to='/home' /> : <RegisterPage />} />
           <Route path='/interests' element={<InterestsSelectionPage />} />
           <Route path='/profile-name' element={<ProfileNameSurnamePage />} />
           <Route path='/profile-creation' element={<ProfileCreation />} />
-          <Route
-            path='/home'
-            element={
-              <ProtectedRoute>
-                <Homepage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/post-details'
-            element={
-              <ProtectedRoute>
-                <PostDetails />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/profile-details'
-            element={
-              <ProtectedRoute>
-                <ProfileDetails />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/me'
-            element={
-              <ProtectedRoute>
-                <MyProfile />
-              </ProtectedRoute>
-            }
-          />
+          <Route path='/home' element={<Homepage />} />
+          <Route path='/post-details' element={<PostDetails />} />
+          <Route path='/profile-details' element={<ProfileDetails />} />
+          <Route path='/me' element={<MyProfile />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
   )
-}
-ProtectedRoute.propTypes = {
-  children: PropTypes.node.isRequired
 }
 
 export default App
